e2cbf5a888680b511a4a8ad92284696ccb027044
ItsProyal <68867736+ItsProyal@users.noreply.github.com>
2020-11-03 15:54:25 -0500

    Create News.lua

---
+++ JSON/News.lua
@@ -0,0 +1,28 @@
+local HttpService = game:GetService('HttpService')
+
+local News = game:HttpGet('https://raw.githubusercontent.com/ItsProyal/SapphireHub/main/JSON/News.json')
+local NewsTable = HttpService:JSONDecode(News)
+
+local allSkins = {
+}
+
+for i, v in pairs(NewsTable)do
+    Title = v
+    for a, b in pairs(v)do
+        if a == "Date" then
+            Date = b
+        elseif a == "Description" then
+            Desc = b
+        end
+    end
+addNews(Title, Date, Desc)
+end
+
+
+
+
+function addNews(title, date, desc)
+    totalInfo = (title.." | "..date.."\n"..desc)
+    SettingsNews:Cheat("Label", totalInfo)
+    SettingsNews:Cheat("Label", "")
+end
